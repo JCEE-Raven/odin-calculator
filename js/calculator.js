8002759162408e74c64ad39f7751ecbed504d3c0
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-07-29 12:14:06 +0100

    Add & Subtract functions created

--- js/calculator.js
+++ js/calculator.js
@@ -2,3 +2,25 @@
 // foundation course final assignment
 //v0.1 Jaycee Raven 29-7-24
 
+//Variables
+let firstNumber = 0,
+    secondNumber = 0,
+    operator = null;
+
+
+//Functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+
+//testing!!
+console.log('add function 1+1')
+console.log(add(firstNumber = 1, secondNumber = 1));
+
+console.log('subtract function 2-1')
+console.log(subtract(firstNumber = 2, secondNumber = 1));
