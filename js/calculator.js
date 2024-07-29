8fcb3818b25c449a78a37a799f85d58e91a73548
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-07-29 15:08:05 +0100

    Multiply and divide functions
    Divide function checks if its trying to divide by 0 if it is returns a
    message say 'Don't Do That!!'

--- js/calculator.js
+++ js/calculator.js
@@ -17,6 +17,18 @@ function subtract(num1, num2) {
     return num1 - num2;
 }
 
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if(secondNumber === 0) {
+        return 'Don\'t do that!!'
+    }
+    
+    return num1 / num2
+}
+
 
 //testing!!
 console.log('add function 1+1')
@@ -24,3 +36,11 @@ console.log(add(firstNumber = 1, secondNumber = 1));
 
 console.log('subtract function 2-1')
 console.log(subtract(firstNumber = 2, secondNumber = 1));
+
+console.log('multiply function 10*2')
+console.log(multiply(firstNumber = 10, secondNumber = 2));
+
+console.log('divide function 10/2')
+console.log(divide(firstNumber = 10, secondNumber = 2));
+console.log('divide by 0?')
+console.log(divide(firstNumber = 10, secondNumber = 0))
