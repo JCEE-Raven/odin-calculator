ecae6c6747ee0e0595e861a0dd9f55018dec0189
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-07-29 15:35:18 +0100

    Add operate function moved all other to here
    Created 1 function that returns the sum of the calculations rather than
    have this call separate calculations functions so i've moved them into
    that function have commented out the other functions but cz\n be
    reverted back if needs be but consider deleting them moving forward

--- js/calculator.js
+++ js/calculator.js
@@ -7,40 +7,59 @@ let firstNumber = 0,
     secondNumber = 0,
     operator = null;
 
-
 //Functions
-function add(num1, num2) {
-    return num1 + num2;
-}
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
+// function subtract(num1, num2) {
+//     return num1 - num2;
+// }
 
-function multiply(num1, num2) {
-    return num1 * num2;
-}
+// function multiply(num1, num2) {   //TEST FUNCTIONS
+//     return num1 * num2;           //Moved functionality
+// }                                 //Leaving here for now delete later
 
-function divide(num1, num2) {
-    if(secondNumber === 0) {
-        return 'Don\'t do that!!'
-    }
+// function divide(num1, num2) {
+//     if(secondNumber === 0) {
+//         return 'Don\'t do that!!'
+//     }
     
-    return num1 / num2
+//     return num1 / num2
+// }
+
+function operate(num1, num2, op) {
+    switch (op) {
+        case '+':
+            return num1 + num2;
+            break;
+        case '-':
+            return num1 - num2;
+            break;
+        case '*':
+            return num1 * num2;
+            break;
+        case '/':
+            if(secondNumber === 0) {
+                return 'Don\'t do that!!'
+            }
+            return num1 / num2
+            break;
+    }
 }
 
 
 //testing!!
 console.log('add function 1+1')
-console.log(add(firstNumber = 1, secondNumber = 1));
+console.log(operate(firstNumber = 1, secondNumber = 1, '+'));
 
 console.log('subtract function 2-1')
-console.log(subtract(firstNumber = 2, secondNumber = 1));
+console.log(operate(firstNumber = 2, secondNumber = 1, '-'));
 
 console.log('multiply function 10*2')
-console.log(multiply(firstNumber = 10, secondNumber = 2));
+console.log(operate(firstNumber = 10, secondNumber = 2, '*'));
 
 console.log('divide function 10/2')
-console.log(divide(firstNumber = 10, secondNumber = 2));
+console.log(operate(firstNumber = 10, secondNumber = 2, '/'));
 console.log('divide by 0?')
-console.log(divide(firstNumber = 10, secondNumber = 0))
+console.log(operate(firstNumber = 10, secondNumber = 0, '/'))
