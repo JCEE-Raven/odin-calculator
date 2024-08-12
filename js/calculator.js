c0dd0a94be04be56e6c644937d30195c8e452fe8
JCEE-Raven <170010059+JCEE-Raven@users.noreply.github.com>
2024-08-12 15:08:39 +0100

    testing code formatter prettier in vs code

--- js/calculator.js
+++ js/calculator.js
@@ -1,132 +1,130 @@
-//a calculator webpage built for the odin project 
+//a calculator webpage built for the odin project
 // foundation course final assignment
 //v0.2 Jaycee Raven 29-7-24
 
 //variables
-let currentNum = '',
-    previousNum = '',
-    resultFlag = false,
-    decimalFlag = false,
-    operator = '';
+let currentNum = "",
+  previousNum = "",
+  resultFlag = false,
+  decimalFlag = false,
+  operator = "";
 
 //display
-const currentDisplayNumber = document.querySelector('.display')
+const currentDisplayNumber = document.querySelector(".display");
 //function buttons
-const equalBTN = document.querySelector('.equal'),
-    decimalBTN = document.querySelector('.decimal'),
-    clearBTN = document.querySelector('.clear');
+const equalBTN = document.querySelector(".equal"),
+  decimalBTN = document.querySelector(".decimal"),
+  clearBTN = document.querySelector(".clear");
 //operator buttons
-const operatorButtons = document.querySelectorAll('.operatorButton');
+const operatorButtons = document.querySelectorAll(".operatorButton");
 //number buttons
-const numberButtons = document.querySelectorAll('.numberButton');
+const numberButtons = document.querySelectorAll(".numberButton");
 
 //event listeners
 numberButtons.forEach((btn) => {
-    btn.addEventListener('click', (event) => {
-        updateDisplay(event.target.textContent);
-        console.log(event.target.textContent)
-    })
-})
+  btn.addEventListener("click", (event) => {
+    updateDisplay(event.target.textContent);
+    console.log(event.target.textContent);
+  });
+});
 
 operatorButtons.forEach((btn) => {
-    btn.addEventListener('click', (event) => {
-        assignOperator(event.target.textContent);
-    })
-})
+  btn.addEventListener("click", (event) => {
+    assignOperator(event.target.textContent);
+  });
+});
 
-equalBTN.addEventListener('click', operate);
+equalBTN.addEventListener("click", operate);
 
-clearBTN.addEventListener('click', clearDisplay);
+clearBTN.addEventListener("click", clearDisplay);
 
-decimalBTN.addEventListener('click', decimalCheck);
+decimalBTN.addEventListener("click", decimalCheck);
 
 //Functions
 function updateDisplay(number) {
-    console.log('in updateDisplay')
-    if(resultFlag == true && operator === '') {
-        currentNum = '';
-        currentDisplayNumber.textContent = currentNum;
-        resultFlag = false;
-    }
-    if(currentNum.length <= 11)
-    {
-        console.log(currentNum);
-        currentNum += number;
-        console.log(currentNum);
-        currentDisplayNumber.textContent = currentNum;
-        console.log(currentNum);
-    }
+  console.log("in updateDisplay");
+  if (resultFlag == true && operator === "") {
+    currentNum = "";
+    currentDisplayNumber.textContent = currentNum;
+    resultFlag = false;
+  }
+  if (currentNum.length <= 11) {
+    console.log(currentNum);
+    currentNum += number;
+    console.log(currentNum);
+    currentDisplayNumber.textContent = currentNum;
+    console.log(currentNum);
+  }
 }
 
 function assignOperator(op) {
-    operator = op;
-    previousNum = currentNum;
-    currentNum = '';
-    currentDisplayNumber.textContent = operator;
+  operator = op;
+  previousNum = currentNum;
+  currentNum = "";
+  currentDisplayNumber.textContent = operator;
 }
 
 function operate() {
-    currentNum = Number(currentNum);
-    previousNum = Number(previousNum);
+  currentNum = Number(currentNum);
+  previousNum = Number(previousNum);
 
-    switch (operator) {
-        case '+':
-            previousNum += currentNum;
-            displayResult();
-            break;
-        case '-':
-            previousNum -= currentNum;
-            displayResult();
-            break;
-        case '*':
-            previousNum *= currentNum;
-            displayResult();
-            break;
-        case '/':
-            if(currentNum === 0) {
-                currentDisplayNumber.textContent = 'Don\'t do that!!';
-                resetGlobals
-                return;
-            }
-            previousNum /= currentNum;
-            displayResult();
-            break;
-    }
-    previousNum = previousNum.toString();
-    currentNum = currentNum.toString();
+  switch (operator) {
+    case "+":
+      previousNum += currentNum;
+      displayResult();
+      break;
+    case "-":
+      previousNum -= currentNum;
+      displayResult();
+      break;
+    case "*":
+      previousNum *= currentNum;
+      displayResult();
+      break;
+    case "/":
+      if (currentNum === 0) {
+        currentDisplayNumber.textContent = "Don't do that!!";
+        resetGlobals;
+        return;
+      }
+      previousNum /= currentNum;
+      displayResult();
+      break;
+  }
+  previousNum = previousNum.toString();
+  currentNum = currentNum.toString();
 }
 
 function displayResult() {
-    resultFlag = true;
-    currentDisplayNumber.textContent = previousNum;
-    currentNum = previousNum;
-    previousNum = '';
-    operator = '';
-    decimalFlag = false;
-    
+  resultFlag = true;
+  currentDisplayNumber.textContent = previousNum;
+  currentNum = previousNum;
+  previousNum = "";
+  operator = "";
+  decimalFlag = false;
 }
 
 function clearDisplay() {
-    currentDisplayNumber.textContent = '';
-    resetGlobals();
+  currentDisplayNumber.textContent = "";
+  resetGlobals();
 }
 
-function decimalCheck () {
-    if(decimalFlag == true && previousNum.includes('.')) {
-        decimalFlag = false;
-    }
+function decimalCheck() {
+  if (decimalFlag == true && previousNum.includes(".")) {
+    decimalFlag = false;
+  }
 
-    if(decimalFlag == false) {
-        decimalFlag = true;
-        currentNum += '.'
-        currentDisplayNumber.textContent = currentNum
-    }
+  if (decimalFlag == false) {
+    decimalFlag = true;
+    currentNum += ".";
+    currentDisplayNumber.textContent = currentNum;
+  }
 }
 
 function resetGlobals() {
-    currentNum = '',
-    previousNum = '',
-    resultFlag = false,
-    decimalFlag = false,
-    operator = '';
+  (currentNum = ""),
+    (previousNum = ""),
+    (resultFlag = false),
+    (decimalFlag = false),
+    (operator = "");
 }
